d8e28008d1492661ec328f094f47b1528417117e
glproj <glprojetinho@gmail.com>
2023-02-20 15:12:02 -0300

    fix: stop pasting with scroll_lock

--- source/keyboard_shortcuts.py
+++ source/keyboard_shortcuts.py
@@ -12,14 +12,7 @@ def on_press(key):
         k = key.char
     except:
         k = key.name
-    if k == "scroll_lock":
-        primary = root.selection_get(selection="PRIMARY")
-        pyperclip.copy(primary)
-        KEY_SIMULATOR.press(keyboard.Key.ctrl)
-        KEY_SIMULATOR.press("v")
-        KEY_SIMULATOR.release(keyboard.Key.ctrl)
-        KEY_SIMULATOR.release("v")
-    elif k == "pause":
+    if k == "pause":
         try:
             image_path = absolute_file_paths(IMAGE_DIR)[0]
             copy_image_then_delete(image_path)
