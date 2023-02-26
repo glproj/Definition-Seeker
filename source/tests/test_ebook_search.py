77754aceede8cec6f0139cd9f2382307fac39f11
glproj <glprojetinho@gmail.com>
2023-02-25 21:13:50 -0300

    test: add more testing to get_examples

--- source/tests/test_ebook_search.py
+++ source/tests/test_ebook_search.py
@@ -1,27 +1,28 @@
 import sys, pathlib
+
 sys.path.append(str(pathlib.Path(__file__).parent.parent))
 from unittest import TestCase, main
 from ebook_search import *
 
 
 class EbookSearchTestCase(TestCase):
+    def setUp(self):
+        self.text = """Und wenn er zu Mittage schläft,
+Sich nicht das Blatt am Zweige regt;
+Gesunder Pflanzen Balsamduft
+Erfüllt die schweigsam stille Luft;
+Die Nymphe darf nicht munter sein,
+Und wo sie stand, da schläft sie ein."""
     def test_get_examples(self):
-        text = """Da ich mit der Geschwulst am Halse sehr geplagt war, indem Arzt und Chirurgus diese Exkreszenz erst vertreiben, hernach, wie sie sagten, zeitigen wollten, und sie zuletzt aufzuschneiden für gut befanden; so hatte ich eine geraume Zeit mehr an Unbequemlichkeit als an Schmerzen zu leiden, obgleich gegen das Ende der Heilung das immer fortdauernde Betupfen mit Höllenstein und andern ätzenden Dingen höchst verdrießliche Aussichten auf jeden neuen Tag geben mußte.
-
-
-      Gewalt ist eher mit Gewalt zu vertreiben; aber ein gut gesinntes, zur Liebe und Teilnahme geneigtes Kind weiß dem Hohn und dem bösen Willen wenig entgegenzusetzen.
-
-
-      Von euch Schurken keinen Spott!
-    Ich tät euch Eseln eine Ehr an,
-    Wie mein Vater Jupiter vor mir getan;
-    Wollt eure dummen Köpf belehren
-    Und euren Weibern die Mücken wehren,
-    Die ihr nicht gedenkt ihnen zu vertreiben;
-    So mögt ihr denn im Dreck bekleiben."""
-        result = get_examples("vertreiben", text)
-        self.assertEqual(len(result), 3)
-
+        with self.subTest("not separable"):
+            result = get_examples("Luft", self.text)
+            self.assertEqual(len(result), 1)
+        with self.subTest("separable"):
+            result = get_examples("schläft ein", self.text)
+            self.assertEqual(len(result), 1)
+        with self.subTest("case sensitivity"):
+            result = get_examples("und", self.text)
+            self.assertEqual(len(result), 1)
 
 if __name__ == "__main__":
     main()
