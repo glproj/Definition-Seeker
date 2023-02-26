e29597b51da8929c2b9d1ac1880fb39238551739
glproj <glprojetinho@gmail.com>
2023-02-25 21:14:29 -0300

    test: test if get_examples highlights the found words

--- source/tests/test_ebook_search.py
+++ source/tests/test_ebook_search.py
@@ -5,6 +5,11 @@ from unittest import TestCase, main
 from ebook_search import *
 
 
+def get_colored_text(text):
+    pattern = re.compile(r"\x1b\[31m(.+?)\x1b\[0m")
+    return re.findall(pattern, text)
+
+
 class EbookSearchTestCase(TestCase):
     def setUp(self):
         self.text = """Und wenn er zu Mittage schläft,
@@ -13,6 +18,7 @@ Gesunder Pflanzen Balsamduft
 Erfüllt die schweigsam stille Luft;
 Die Nymphe darf nicht munter sein,
 Und wo sie stand, da schläft sie ein."""
+
     def test_get_examples(self):
         with self.subTest("not separable"):
             result = get_examples("Luft", self.text)
@@ -23,6 +29,17 @@ Und wo sie stand, da schläft sie ein."""
         with self.subTest("case sensitivity"):
             result = get_examples("und", self.text)
             self.assertEqual(len(result), 1)
+
+    def test_colored_words(self):
+        with self.subTest("not separable"):
+            examples = get_examples("Luft", self.text)
+            result = get_colored_text(next(iter(examples)))
+            self.assertEqual(result, ["Luft"])
+        with self.subTest("separable"):
+            examples = get_examples("schläft ein", self.text)
+            result = get_colored_text(next(iter(examples)))
+            self.assertEqual(result, ["schläft", "ein"])
+
 
 if __name__ == "__main__":
     main()
