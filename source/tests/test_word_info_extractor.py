3452fd83428549fe74d37dc0d4d04a39157f2214
glproj <glprojetinho@gmail.com>
2023-03-12 15:03:08 -0300

    test: fix tests in test_word_info_extractor.py

--- source/tests/test_word_info_extractor.py
+++ source/tests/test_word_info_extractor.py
@@ -1,4 +1,5 @@
 import sys, pathlib
+
 sys.path.append(str(pathlib.Path(__file__).parent.parent))
 from unittest import TestCase, main
 from word_info_extractor import *
@@ -9,41 +10,42 @@ from word_info_extractor import *
 # ! = not
 WORDS = (
     "ging",  # verb wmb/!bf
-    # "lästern",  # verb wmb/bf
+    "lästern",  # verb wmb/bf
     "Stuhl",  # noun wmb/bf
-    # "durchschneiden",  # verb !wmb/bf
+    "durchschneiden",  # verb !wmb/bf
     "gesucht",  # not a base form but has its own definition in wikipedia
-    # "verfahren",  # Verb and Adjektiv at the same time
-    # "Junge",  # weird table compared to other nouns. It's also a surname
+    "verfahren",  # Verb and Adjektiv at the same time
+    "Junge",  # weird table compared to other nouns. It's also a surname
     "ja",  # present in multiple languages
 )
 
 
-class UtilsTestCase(TestCase):
-    def test_create_duden_compatible(self):
-        self.assertEqual("aeoeuePAeOeUe", create_duden_compatible("äöüPÄÖÜ"))
+class BaseTestCases:
+    class BaseTestCase(TestCase):
+        @classmethod
+        def setUpClass(cls):
+            word_to_instance_dict = {}
+            for word in WORDS:
+                try:
+                    instance = cls.class_(word)
+                except WordNotAvailable:
+                    print(
+                        f"{cls.class_.__name__} returned error with argument '{word}'"
+                    )
+                word_to_instance_dict.update({word: instance})
+            cls.word_to_instance_dict = word_to_instance_dict
+            cls.t_class = DEWiktionaryWord
 
-class BaseWordTestCase(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        word_to_instance_dict = {}
-        for word in WORDS:
-            print(word)
-            instance = DEWiktionaryWord(word)
-            word_to_instance_dict.update({word: instance})
-        cls.word_to_instance_dict = word_to_instance_dict
-        cls.t_class = DEWiktionaryWord
+        def test_get_info(self):
+            words = ["Stuhl"]
+            for word in words:
+                with self.subTest(word):
+                    result = self.word_to_instance_dict[word].root_info
+                    self.assertIn("[1]", result)
 
-    def test_get_info(self):
-        words = ("gesucht", "Stuhl")
-        g_infos = ("Adjektiv", "Substantiv, m")
-        for word, g_info in zip(words, g_infos):
-            with self.subTest(word):
-                result = next(self.word_to_instance_dict[word].root_info)
-                self.assertIn("[1]", result)
-                self.assertIn(g_info, result)
 
-class WiktionaryCase(TestCase):
+class WiktionaryTestCase(BaseTestCases.BaseTestCase, TestCase):
+    class_ = DEWiktionaryWord
 
     def test_is_inflection_without_own_definition(self):
         with self.subTest("ging"):
@@ -70,24 +72,24 @@ class WiktionaryCase(TestCase):
         self.assertTrue(root_page.find(title="Sinn und Bezeichnetes (Semantik)"))
 
     def test_root(self):
-        root= self.word_to_instance_dict["ging"].root
+        root = self.word_to_instance_dict["ging"].root
         self.assertEqual(root, "gehen")
 
     def test_unavailable_word(self):
-        with self.assertRaises(NotAvailableAtWiktionary):
+        with self.assertRaises(WordNotAvailable):
             DEWiktionaryWord("bruh123")
 
     def test_get_pronunciation(self):
         pronunciation_url = self.word_to_instance_dict["Stuhl"].pronunciation_url
         ipa = self.word_to_instance_dict["Stuhl"].root_ipa
         self.assertEqual(
-            f'{ipa} {pronunciation_url}',
+            f"{ipa} {pronunciation_url}",
             "ʃtuːl https://upload.wikimedia.org/wikipedia/commons/3/38/De-Stuhl.ogg",
         )
 
     def test_get_info_wiktionary(self):
         pass
-    
+
     def test_only_de(self):
         ja_page = self.word_to_instance_dict["ja"].root_page
         self.assertTrue(ja_page.find(href="/wiki/Wiktionary:Deutsch"))
@@ -95,28 +97,18 @@ class WiktionaryCase(TestCase):
         self.assertFalse(ja_page.find(href="/wiki/Wiktionary:F%C3%A4r%C3%B6isch"))
 
 
-class DWDSTestCase(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        word_to_instance_dict = {}
-        for word in WORDS:
-            word_to_instance_dict.update({word: DWDSWord(word)})
-        cls.word_to_instance_dict = word_to_instance_dict
+class DWDSTestCase(BaseTestCases.BaseTestCase, TestCase):
+    class_ = DWDSWord
 
     def test_get_info_dwds(self):
-        page = self.word_to_instance_dict["Stuhl"].info
+        page = self.word_to_instance_dict["Stuhl"].root_info
         self.assertIn("bildlich", page)
-        self.assertIn("Beispiel", page)
-        self.assertFalse("Stuhl" in page)
+        self.assertIn("https://media.dwds.de/dwds2/audio/004/der_Stuhl.mp3", page)
+        self.assertIn("Stuhl", page)
 
 
-class DudenTestCase(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        word_to_page_dict = {}
-        for word in WORDS:
-            word_to_page_dict.update({word: DudenWord(word)})
-        cls.word_to_page_dict = word_to_page_dict
+class DudenTestCase(BaseTestCases.BaseTestCase, TestCase):
+    class_ = DudenWord
 
 
 if __name__ == "__main__":
