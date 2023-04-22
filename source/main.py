f79438aa01258d117f3fa87c9da37dd35166735f
glproj <glprojetinho@gmail.com>
2023-04-22 10:16:25 -0300

    fix: prevent Keyerror when importing programs

--- source/main.py
+++ source/main.py
@@ -2,7 +2,6 @@ import keyboard_shortcuts, requests, bs4, pyperclip, os, ast, readline, atexit, 
 from word_info_extractor import *
 from image_extractor import *
 from ebook_search import *
-import programs
 from pathlib import Path
 from config_setter import ConfigApp, setup_initial_config_file
 
@@ -29,14 +28,14 @@ def save(prev_h_len, histfile):
 
 def save_when_ctrl_c(signum, frame):
     save(h_len, histfile)
-    print('\033[0m', end='')
+    print("\033[0m", end="")
     sys.exit()
 
 
 signal.signal(signal.SIGINT, save_when_ctrl_c)
 atexit.register(save, h_len, histfile)
 atexit.register(remove_q)
-atexit.register(print, '\033[0m')
+atexit.register(print, "\033[0m")
 # --- exit handlers --
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("-w", "--word", help="The word you are trying to search")
@@ -50,7 +49,9 @@ arg_parser.add_argument(
 arg_parser.add_argument("-c", "--config", action="store_true")
 args = arg_parser.parse_args()
 str_valid_lc = ", ".join(VALID_LANGUAGE_CODES)
-if not CONFIG_PARSER.get("DEFAULT", "language", fallback=False) or vars(args).get("config"):
+if not CONFIG_PARSER.get("DEFAULT", "language", fallback=False) or vars(args).get(
+    "config"
+):
     if not vars(args).get("config"):
         setup_initial_config_file()
         print(
@@ -59,11 +60,15 @@ change your settings later with the --config option"""
         )
         print()
     config_app = ConfigApp()
-    config_app.geometry('500x500')
+    config_app.geometry("500x500")
     config_app.mainloop()
-    CONFIG_PARSER["DEFAULT"]['google_search_api_key'] = input("Google Search Api Key (press 'enter' if you don't have one): ")
-    CONFIG_PARSER["DEFAULT"]['cx'] = input("Google Search Engine ID (press 'enter' if you don't have one): ")
-    with open(CONFIG_PATH, 'w') as config:
+    CONFIG_PARSER["DEFAULT"]["google_search_api_key"] = input(
+        "Google Search Api Key (press 'enter' if you don't have one): "
+    )
+    CONFIG_PARSER["DEFAULT"]["cx"] = input(
+        "Google Search Engine ID (press 'enter' if you don't have one): "
+    )
+    with open(CONFIG_PATH, "w") as config:
         CONFIG_PARSER.write(config)
 
 print(
@@ -81,6 +86,7 @@ print("-" * 72)
 setup_ebooks()
 classes = {'br': BRDicioWord,'en': ENDictionaryWord, 'de': DEWiktionaryWord}
 language = CONFIG_PARSER["DEFAULT"]["language"]
+import programs
 program = programs.Program()
 program.word_class = classes[language]
 program.cmdloop()
