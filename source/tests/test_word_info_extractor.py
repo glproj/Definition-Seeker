a40342c101efb0d20cfd3eaac27ad735013be801
glproj <glprojetinho@gmail>
2024-01-30 14:03:06 -0300

    test: test every word class

--- source/tests/test_word_info_extractor.py
+++ source/tests/test_word_info_extractor.py
@@ -4,47 +4,44 @@ sys.path.append(str(pathlib.Path(__file__).parent.parent))
 from unittest import TestCase, main
 from word_info_extractor import *
 
-# wmb = without multiple Bedeutung sections
-# bf = already in the base form
-# mrw = multiple redirects until base form is reached
-# ! = not
-WORDS = (
-    "ging",  # verb wmb/!bf
-    "lästern",  # verb wmb/bf
-    "Stuhl",  # noun wmb/bf
-    "durchschneiden",  # verb !wmb/bf
-    "gesucht",  # not a base form but has its own definition in wikipedia
-    "verfahren",  # Verb and Adjektiv at the same time
-    "Junge",  # weird table compared to other nouns. It's also a surname
-    "ja",  # present in multiple languages
-)
-
-
 class BaseTestCases:
     class BaseTestCase(TestCase):
+        words = []
+
         @classmethod
         def setUpClass(cls):
             word_to_instance_dict = {}
-            for word in WORDS:
-                try:
-                    instance = cls.class_(word)
-                except WordNotAvailable:
-                    print(
-                        f"{cls.class_.__name__} returned error with argument '{word}'"
-                    )
+            for word in cls.words:
+                instance = cls.class_(word)
                 word_to_instance_dict.update({word: instance})
             cls.word_to_instance_dict = word_to_instance_dict
             cls.t_class = DEWiktionaryWord
 
         def test_get_info(self):
-            words = ["Stuhl"]
-            for word in words:
+            for word in self.words:
                 with self.subTest(word):
                     result = self.word_to_instance_dict[word].root_info
-                    self.assertIn("[1]", result)
+                    self.assertIsNot("", result)
+        def test_error(self):
+            with self.assertRaises(WordNotAvailable):
+                self.class_("fjdkslafjdsklfjldsçfjksdalfjfjkdslaçfjdskla")
 
 
-class WiktionaryTestCase(BaseTestCases.BaseTestCase, TestCase):
+class WiktionaryTestCase(BaseTestCases.BaseTestCase):
+    # wmb = without multiple Bedeutung sections
+    # bf = already in the base form
+    # mrw = multiple redirects until base form is reached
+    # ! = not
+    words = (
+        "ging",  # verb wmb/!bf
+        "lästern",  # verb wmb/bf
+        "Stuhl",  # noun wmb/bf
+        "durchschneiden",  # verb !wmb/bf
+        "gesucht",  # not a base form but has its own definition in wikipedia
+        "verfahren",  # Verb and Adjektiv at the same time
+        "Junge",  # weird table compared to other nouns. It's also a surname
+        "ja",  # present in multiple languages
+    )
     class_ = DEWiktionaryWord
 
     def test_is_inflection_without_own_definition(self):
@@ -97,18 +94,60 @@ class WiktionaryTestCase(BaseTestCases.BaseTestCase, TestCase):
         self.assertFalse(ja_page.find(href="/wiki/Wiktionary:F%C3%A4r%C3%B6isch"))
 
 
-class DWDSTestCase(BaseTestCases.BaseTestCase, TestCase):
+class DWDSTestCase(BaseTestCases.BaseTestCase):
+    words = (
+        "ging",  # verb wmb/!bf
+        "lästern",  # verb wmb/bf
+        "Stuhl",  # noun wmb/bf
+        "durchschneiden",  # verb !wmb/bf
+        "gesucht",  # not a base form but has its own definition in wikipedia
+        "verfahren",  # Verb and Adjektiv at the same time
+        "Junge",  # weird table compared to other nouns. It's also a surname
+        "ja",  # present in multiple languages
+    )
     class_ = DWDSWord
 
     def test_get_info_dwds(self):
         page = self.word_to_instance_dict["Stuhl"].root_info
         self.assertIn("bildlich", page)
-        self.assertIn("https://media.dwds.de/dwds2/audio/004/der_Stuhl.mp3", page)
+        self.assertIn("der_Stuhl.mp3", page)
         self.assertIn("Stuhl", page)
 
 
-class DudenTestCase(BaseTestCases.BaseTestCase, TestCase):
+class DudenTestCase(BaseTestCases.BaseTestCase):
+    words = (
+        "lästern",  # verb wmb/bf
+        "Stuhl",  # noun wmb/bf
+        "gesucht",  # not a base form but has its own definition in wikipedia
+        "Junge",  # weird table compared to other nouns. It's also a surname
+        "ja",  # present in multiple languages
+    )
     class_ = DudenWord
+
+
+class BRDicioWordTestCase(BaseTestCases.BaseTestCase):
+    words = ("bola", "mamão", "paralelepípedo", "meio-dia")
+    class_ = BRDicioWord
+
+
+class ENDictionaryTestCase(BaseTestCases.BaseTestCase):
+    words = ("scant", "one-way")
+    class_ = ENDictionaryWord
+
+
+class ENRUWiktionaryTestCase(BaseTestCases.BaseTestCase):
+    words = ("твое", "твой", "Санкт-Петербург")
+    class_ = ENRUWiktionaryWord
+
+
+class FRWitionaryTestCase(BaseTestCases.BaseTestCase):
+    words = ("milieu", "cigale", "été", "de_nouveau")
+    class_ = FRWitionaryWord
+
+
+class LAWiktionaryTestCase(BaseTestCases.BaseTestCase):
+    words = ("cum", "communio")
+    class_ = LAWiktionaryWord
 
 
 if __name__ == "__main__":
