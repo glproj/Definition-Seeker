509758b7f447be96a39c457fc7b546e9a6a88776
glproj <glprojetinho@gmail>
2024-01-31 08:59:33 -0300

    test: test alternative sources

--- source/tests/test_programs.py
+++ source/tests/test_programs.py
@@ -1,9 +1,9 @@
 import io, sys, pathlib
+from logging import log
 from unittest import TestCase, main
 
 sys.path.append(str(pathlib.Path(__file__).parent.parent))
 from programs import *
-import word_info_extractor
 from ipdb import set_trace as s
 
 
@@ -12,7 +12,7 @@ class ProgramTestCase(TestCase):
         self.test_in = io.StringIO()
         self.test_out = io.StringIO()
         self.cmd = TestProgram(stdout=self.test_out, stdin=self.test_in)
-        self.cmd.word_class = word_info_extractor.DEWiktionaryWord
+        self.cmd.preloop()
 
     def test_basic(self):
         self.inp("Haus")
@@ -21,7 +21,7 @@ class ProgramTestCase(TestCase):
 
     def test_examples(self):
         self.inp("examples oder")
-        self.assertIn("Ball", self.output)
+        self.assertIn("oder", self.output)
         ebook_names = self.cmd.ebook_lang_name_txt["de"]
         ebook_names = [
             (name, txt)
@@ -30,7 +30,29 @@ class ProgramTestCase(TestCase):
         ]
         for ebook_name, _ in ebook_names:
             self.assertIn(ebook_name.upper(), self.output)
-    
+
+    def test_other_sources(self):
+        with self.subTest("en"):
+            self.inp("lang en")
+            self.inp("ball")
+            self.test_out.truncate(0)
+            self.test_out.seek(0)
+            self.inp("dict")
+            self.assertIn("noun", self.output)
+        with self.subTest("de"):
+            self.inp("lang de")
+            self.inp("Ball")
+            self.test_out.truncate(0)
+            self.test_out.seek(0)
+            self.inp("dwds")
+            self.assertNotIn("noun", self.output)
+            self.assertIn("kugelförmiges", self.output)
+    def test_other_sources_wrong_language(self):
+        self.inp("lang de")
+        self.inp("Huhn")
+        self.inp("dict")
+        self.assertIn("Change your language", self.output)
+
 
     def inp(self, text: str):
         """write in self.test_in and run a cmdloop in self.cmd
@@ -38,8 +60,9 @@ class ProgramTestCase(TestCase):
         Args:
             text (str): input
         """
-        self.test_in.write(text)
-        self.cmd.cmdloop()
+        line = self.cmd.precmd(text)
+        stop = self.cmd.onecmd(line)
+        stop = self.cmd.postcmd(stop, line)
 
     @property
     def output(self):
